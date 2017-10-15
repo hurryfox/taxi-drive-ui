b7dcab52ec98160f0fcbc64594c9a2763c7303c8
MarkKlimenko <22773@bk.ru>
2017-10-15 20:36:20 +1000

    Add KC support | rename person to client

--- src/app/clients-table/clients-table.component.ts
+++ src/app/clients-table/clients-table.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {NgForm} from "@angular/forms";
 
+declare const keycloak:any;
+
 @Component({
   selector: 'app-clients-table',
   templateUrl: './clients-table.component.html',
@@ -13,15 +15,19 @@ export class ClientsTableComponent implements OnInit  {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('http://localhost:8087/api/person/all').subscribe(data => {
+    let headers = new HttpHeaders().set('Authorization', 'Bearer ' + keycloak.token);
+
+    this.http.get('http://vostok.systems/tda/api/client/all', {headers: headers}).subscribe(data => {
       this.clients = data;
     });
   }
 
   onSubmit(form: NgForm) {
+
+
     console.log('OK');
-    let headers = new HttpHeaders().set('Content-Type', 'application/json');
-    let url = 'http://localhost:8087/api/person';
+    let headers = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', 'Bearer ' + keycloak.token);
+    let url = 'http://vostok.systems/tda/api/client';
 
     this.http.put(url, JSON.stringify(form.value), {headers: headers}).subscribe(
       res => {
