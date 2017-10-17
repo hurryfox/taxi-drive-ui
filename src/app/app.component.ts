5f92dd47bd8fc5a6485bff9ec0f3e1d1ac7a64bb
MarkKlimenko <22773@bk.ru>
2017-10-17 20:08:29 +1000

    Add header functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 
+declare const keycloak:any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +10,14 @@ import {Component} from '@angular/core';
 
 export class AppComponent {
   title = 'app';
+
+  logout() {
+    keycloak.logout();
+  }
+
+  getUsername() {
+    return keycloak.idTokenParsed.preferred_username
+  }
 }
 
 
--- src/app/clients-table/clients-table.component.ts
+++ src/app/clients-table/clients-table.component.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {NgForm} from "@angular/forms";
 
 declare const keycloak:any;
+declare const globalUrl:any;
 
 @Component({
   selector: 'app-clients-table',
@@ -15,9 +16,9 @@ export class ClientsTableComponent implements OnInit  {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    let headers = new HttpHeaders().set('Authorization', 'Bearer ' + keycloak.token);
+    let headers = new HttpHeaders(); //.set('Authorization', 'Bearer ' + keycloak.token);
 
-    this.http.get('http://vostok.systems/tda/api/client/all', {headers: headers}).subscribe(data => {
+    this.http.get(globalUrl + '/api/client/all', {headers: headers}).subscribe(data => {
       this.clients = data;
     });
   }
@@ -26,8 +27,8 @@ export class ClientsTableComponent implements OnInit  {
 
 
     console.log('OK');
-    let headers = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', 'Bearer ' + keycloak.token);
-    let url = 'http://vostok.systems/tda/api/client';
+    let headers = new HttpHeaders().set('Content-Type', 'application/json'); //.set('Authorization', 'Bearer ' + keycloak.token);
+    let url = globalUrl + '/api/client';
 
     this.http.put(url, JSON.stringify(form.value), {headers: headers}).subscribe(
       res => {
