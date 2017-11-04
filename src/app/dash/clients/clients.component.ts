6fb98367ebef4717a08bb16aa0e0e9e3000a7d1c
MarkKlimenko <22773@bk.ru>
2017-11-04 23:58:58 +1000

    Add ride form

--- src/app/dash/clients/clients.component.ts
+++ src/app/dash/clients/clients.component.ts
@@ -66,11 +66,11 @@ import {Component, OnInit} from '@angular/core';
               <div class="alert alert-danger" role="alert">
                 User phone number in DB, but name and surname not provided
               </div>
-              
+
               <div class="alert alert-success" role="alert">
-                The next ride is free, total rides amount <b>9</b> 
+                The next ride is free, total rides amount <b>9</b>
               </div>
-              
+
             </div>
             <div class="col col-md-6">
               <div class="alert alert-info" role="alert">
@@ -84,10 +84,66 @@ import {Component, OnInit} from '@angular/core';
 
       </div>
 
-      <div class="row mt-2 block">
-        <div class="col col-md-12">
-          <h5>Ride board</h5>
+      <div class="mt-2">
+        <div class="row mt-2">
+          <div class="col col-md-4 block">
+            <h5>Ride form</h5>
+          </div>
         </div>
+
+        <div class="row">
+          <div class="col col-md-2 block">
+            <form>
+              <h6>From</h6>
+
+              <div class="form-group">
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="State">
+              </div>
+
+              <div class="form-group">
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="City">
+              </div>
+
+              <div class="form-group">
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Street">
+              </div>
+
+              <div class="form-group">
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Building">
+              </div>
+
+            </form>
+          </div>
+
+          <div class="col col-md-2 block">
+            <form>
+              <h6>To</h6>
+
+              <div class="form-group">
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="State">
+              </div>
+
+              <div class="form-group">
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="City">
+              </div>
+
+              <div class="form-group">
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Street">
+              </div>
+
+              <div class="form-group">
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Building">
+              </div>
+
+              <div class="form-group float-right">
+                  <button type="submit" class="btn btn-primary mr-2 btn-fix-size">Price</button>
+                  <button type="submit" class="btn btn-primary btn-fix-size">Submit</button>
+              </div>
+              
+            </form>
+          </div>
+        </div>
+
       </div>
     </div>
   `
