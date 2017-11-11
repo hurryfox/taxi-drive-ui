99507c631210cb8354b3577ce889a386c2a030b9
MarkKlimenko <22773@bk.ru>
2017-11-11 12:55:29 +1000

    Submit number checking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -49,6 +49,7 @@ import {ActiveModalComponent} from "./dash/clients/rides/active/modal.component"
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AngularFontAwesomeModule,
 
--- src/app/dash/clients/validation/number/number.component.ts
+++ src/app/dash/clients/validation/number/number.component.ts
@@ -10,46 +10,59 @@ import {Component, OnInit} from '@angular/core';
       </div>
     </div>
 
-    <div class="row">
-      <div class="col col-md-3 block">
-        <form>
+    <form #f="ngForm" (ngSubmit)="onSubmit(f.value)">
+      <div class="row">
+        <div class="col col-md-3 block">
           <div class="form-group">
-            <select class="form-control" id="exampleSelect1">
-              <option>+7</option>
-              <option>local</option>
+            <select class="form-control" id="clientNumber" name="clientNumType" [(ngModel)]="selectedNumberCode" (change)="onBannerChange()">
+              <option *ngFor="let code of numberCodes" [ngValue]="code">{{code.numberCode}}</option>
             </select>
           </div>
-        </form>
-      </div>
+        </div>
 
-      <div class="col col-md-9 block">
-        <form>
+        <div class="col col-md-9 block">
           <div class="form-group">
-            <input type="tel" class="form-control" id="exampleInputEmail1" placeholder="Client phone number">
+            <input type="tel" class="form-control" id="exampleInputEmail1" placeholder="Client phone number" name="clientNumber" ngModel>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
-
-    <div class="row">
-      <div class="col col-md-12 block">
-        <form>
+  
+      <div class="row">
+        <div class="col col-md-12 block">
           <div class="form-group float-right">
             <button type="submit" class="btn btn-primary">Submit</button>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
+    </form>
+
   `
 })
 export class ValidationNumberComponent implements OnInit {
+
+  public numberCodes: any[] = [
+    {numberCode: '+7'},
+    {numberCode: 'local'}
+  ];
+
+  public selectedNumberCode: any = this.numberCodes[0];
 
   constructor() {
   }
 
   ngOnInit() {
+    this.selectedNumberCode = this.numberCodes.find(x =>
+                        x.numberCode == this.selectedNumberCode.numberCode);
+
   }
 
+  onBannerChange() {
+    console.log(this.selectedNumberCode);
+  }
+
+  onSubmit(form: any): void {
+    console.log('you submitted value:', form);
+  }
 }
 
 
