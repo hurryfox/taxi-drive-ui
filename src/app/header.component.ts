11587763e5e279d3ce777180e025b5e334cd0e68
MarkKlimenko <22773@bk.ru>
2017-11-12 13:41:04 +1000

    Add fixed header component

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-header',
   styles: [],
   template: `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#">TDA</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
